f9e0abc4ce5b2b6a3ae773452b14f7c7d72f480c
Nate Silva <nate.s.silva@gmail.com>
2019-01-28 13:00:31 -0700

    improved parse logic.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.0.3] - 2019-01-28
+
+- Improved parsing logic
+
 ## [1.0.1] - 2019-01-27
 
 ### Added
--- src/parse.ts
+++ src/parse.ts
@@ -1,33 +1,33 @@
 import * as ts from "typescript";
 
-type dict<T> = {[key:string]:T };
+type dict<T> = { [key: string]: T };
 
-export function parseText(fileText:string){
+export function parseText(fileText: string) {
     let sourceFile = ts.createSourceFile(
         'temp.ts',
         fileText,
         ts.ScriptTarget.ES2015,
         true
-      );
-    
-     return parse(sourceFile);
+    );
+
+    return parse(sourceFile);
 }
 
-function parse(sourceFile: ts.SourceFile){
-    const rows:string[] = [];
-    sourceFile.forEachChild(node=>{
-        if(node.kind = ts.SyntaxKind.ClassDeclaration){
-            const dag:dict<string[]> = {};
+function parse(sourceFile: ts.SourceFile) {
+    const rows: string[] = [];
+    sourceFile.forEachChild(node => {
+        if (node.kind = ts.SyntaxKind.ClassDeclaration) {
+            const dag: dict<string[]> = {};
             let madeIt = false;
-            (<ts.ClassDeclaration>node).forEachChild(node=>{
-                if(node.kind == ts.SyntaxKind.PropertyDeclaration){
+            (<ts.ClassDeclaration>node).forEachChild(node => {
+                if (node.kind == ts.SyntaxKind.PropertyDeclaration) {
                     madeIt = true;
                     parsePropertyDeclaration(dag, <ts.PropertyDeclaration>node);
                 }
             })
-            if(madeIt){
-                for(const prop in dag){
-                    if(dag[prop].length){
+            if (madeIt) {
+                for (const prop in dag) {
+                    if (dag[prop].length) {
                         rows.push(`   { ${dag[prop].join(' ')} } -> ${prop}`);
                     }
                 }
@@ -40,38 +40,30 @@ ${rows.join('\n')}
 `;
 }
 
-function parsePropertyDeclaration(dag:dict<string[]>, prop:ts.PropertyDeclaration){
+function parsePropertyDeclaration(dag: dict<string[]>, prop: ts.PropertyDeclaration) {
     const name = prop.name.getText();
     dag[name] = [];
-    prop.forEachChild(node=>{
-        if(node.kind = ts.SyntaxKind.CallExpression){
-            parseCallExpression(dag,name, <ts.CallExpression>node);
-        }
-    })
+    prop.forEachChild(node => parsePropertyChild(dag, name, node))
 }
 
-function parsePropertyAccessExpression(dag:dict<string[]>, name:string,expr:ts.PropertyAccessExpression){
-    expr.forEachChild(node=>{
-        switch(node.kind){
-            case ts.SyntaxKind.Identifier:
-            if(dag[node.getText()]){
-                dag[name].push(node.getText());
-            }
+function parsePropertyChild(dag: dict<string[]>, name: string, node: ts.Node) {
+    switch (node.kind) {
+        case ts.SyntaxKind.PropertyAccessExpression:
+            parsePropertyAccessExpression(dag, name, <ts.PropertyAccessExpression>node);
             break;
-            case ts.SyntaxKind.PropertyAccessExpression:
-            parsePropertyAccessExpression(dag, name,<ts.PropertyAccessExpression>node);
+        default:
+            node.forEachChild(child => parsePropertyChild(dag, name, child))
             break;
-            case ts.SyntaxKind.CallExpression:
-            parseCallExpression(dag, name,<ts.CallExpression>node);
-            break;
-        }
-    });
+    }
 }
 
-function parseCallExpression(dag:dict<string[]>,name:string, expr:ts.CallExpression){
-    expr.forEachChild(node=>{
-        if(node.kind = ts.SyntaxKind.PropertyAccessExpression){
-           parsePropertyAccessExpression(dag,name, (<ts.PropertyAccessExpression>node));
+function parsePropertyAccessExpression(dag: dict<string[]>, name: string, expr: ts.PropertyAccessExpression) {
+    if (expr.getChildAt(0).kind == ts.SyntaxKind.ThisKeyword && expr.getChildAt(1).kind == ts.SyntaxKind.DotToken) {
+        const identifier = expr.getChildAt(2);
+        if (dag.hasOwnProperty(identifier.getText())) {
+            dag[name].push(identifier.getText());
         }
-    })
+        return;
+    }
+    expr.forEachChild(node => parsePropertyChild(dag, name, node));
 }
